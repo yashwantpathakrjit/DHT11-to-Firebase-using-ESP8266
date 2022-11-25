b1e9f17b98adb6c8863684e7d37752250789d87e
Yashwant pathak <96110574+yashwantpathakrjit@users.noreply.github.com>
2022-11-25 21:49:51 +0530

    Add files via upload

---
+++ dht_firebase.ino
@@ -0,0 +1,111 @@
+#include "DHT.h"
+#define DHTPIN D1
+#define DHTTYPE DHT11
+
+#include <Arduino.h>
+#if defined(ESP32)
+  #include <WiFi.h>
+#elif defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#endif
+#include <Firebase_ESP_Client.h>
+
+DHT dht(DHTPIN, DHTTYPE);
+
+//Provide the token generation process info.
+#include "addons/TokenHelper.h"
+//Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+
+// Insert your network credentials
+#define WIFI_SSID "YPNPAPDP"
+#define WIFI_PASSWORD "11223344"
+
+// Insert Firebase project API Key
+#define API_KEY "AIzaSyA3z7d0VS0nUGtmfY1Bgea88Z9Tf2CHIuE"
+
+// Insert RTDB URLefine the RTDB URL */
+#define DATABASE_URL "https://homeweather-ca8b9-default-rtdb.firebaseio.com/" 
+
+//Define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+//unsigned long sendDataPrevMillis = 0;
+//int count = 0;
+bool signupOK = false;
+
+void setup(){
+  pinMode(DHTPIN, INPUT);
+  dht.begin();
+  Serial.begin(115200);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Sign up */
+  if (Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("ok");
+    signupOK = true;
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+}
+
+void loop(){
+ delay(1000);
+  float h = dht.readHumidity();
+
+  float t = dht.readTemperature();
+  
+  if (Firebase.ready() && signupOK ) {
+    
+    if (Firebase.RTDB.setFloat(&fbdo, h+'B',h)){
+//      Serial.println("PASSED");
+       Serial.print("Humidity: ");
+       Serial.println(h);
+      
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+    
+    
+    // Write an Float number on the database path test/float
+    if (Firebase.RTDB.setFloat(&fbdo, t+'A',t)){
+//      Serial.println("PASSED");
+
+       Serial.print("Temperature: ");
+       
+       Serial.println(t);
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+  }
+  Serial.println("______________________________");
+}
